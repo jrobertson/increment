426695a0aa34cb7cab5afaa6b2701b7fabffc4ca
Your Name <james@r0bertson.co.uk>
2012-05-26 16:48:42 +0100

    replaced a hard-coded file name

--- increment.gemspec
+++ increment.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'increment'
-  s.version = '0.1.0'
+  s.version = '0.1.1'
   s.summary = 'Increments the value of a number in a text file'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
--- lib/increment.rb
+++ lib/increment.rb
@@ -7,7 +7,7 @@ class Increment
   def self.update(file='counter.txt')
 
     if File.exists? file then
-      File.open('test','r+'){|f| i = f.read.succ; f.rewind; f.write i}
+      File.open(file, 'r+'){|f| i = f.read.succ; f.rewind; f.write i}
     else
       File.open(file,'w'){|f| f.write '1'}
     end
